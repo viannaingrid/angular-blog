a67a8056f371a52b1b30a84cb684474ea435d85d
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-10 22:00:34 -0300

    :construction: creating a dataFake(inside db with json)

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,3 +1,20 @@
-export const dataFake = {
-
-}
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description": "ola"
+  },
+  {
+    "id":"2",
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description": "ola"
+  },
+  {
+    "id":"3",
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description": "ola"
+  },
+]
