aa1e7d819b367a434e0d53ad3532acfc6c21888d
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-10 21:45:56 -0300

    :construction: configuration of dinamic data

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent {
-
+  photoCover:string = "https://files.tecnoblog.net/wp-content/uploads/2021/01/o_que_e_php_unsplash-e1612906186814.jpg"
+  contentTitle:string = "Minha noticia"
+  contentDescription:string = "testetstetstettststststststetetesfyasdchghadlygivgab.v.h.hvbhfevhregfuwerqyfuei  fhjhweijbjehwqgfuyibdlvhjhidnvj;hbß"
 }
